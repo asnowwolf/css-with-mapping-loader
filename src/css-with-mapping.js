cd8b707515f07a9675ac73d76896031f32873c2c
Zhicheng Wang <zcwang@thoughtworks.com>
2017-02-03 11:10:11 +0800

    bugfix: can't work properly

--- src/css-with-mapping.js
+++ src/css-with-mapping.js
@@ -1,9 +1,10 @@
 /**
  * @see https://webpack.github.io/docs/loaders.html
  */
-module.exports = function (content) {
+module.exports = function (script) {
+    var content = this.exec(script, __filename);
     if (!content || content.length !== 1) {
-        throw new Error('The css-with-mapping-loader must be chained after the css-loader');
+        throw new Error('The css-with-mapping-loader must be chained after the css-loader\n' + content);
     }
     var cssObject = content[0];
     var cssContent = cssObject[1] || '';
@@ -16,5 +17,5 @@ module.exports = function (content) {
     var sourceURLs = cssMapping.sources.map(function (source) {
         return '/*# sourceURL=' + cssMapping.sourceRoot + source + ' */'
     });
-    return [cssContent].concat(sourceURLs).concat([sourceMapping]).join('\n');
+    return 'module.exports = ' + JSON.stringify([cssContent].concat(sourceURLs).concat([sourceMapping]).join('\n'));
 };
